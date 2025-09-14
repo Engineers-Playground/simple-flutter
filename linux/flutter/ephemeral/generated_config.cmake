5787370a5a0ecb3218d69612a28aa211f7729b75
Domirando <vohidjonovnamaftuna@gmail.com>
2025-09-14 17:58:42 +0500

    feat: to-do app

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/nix/store/f4zf4xz1zngg0d12wsfviiqljsrpxajs-flutter-wrapped-3.35.2-sdk-links" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/domirando/Workspace/playground/flutterchi" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/nix/store/f4zf4xz1zngg0d12wsfviiqljsrpxajs-flutter-wrapped-3.35.2-sdk-links"
+  "PROJECT_DIR=/home/domirando/Workspace/playground/flutterchi"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049bml4cGtnczAwMA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YThiZmRmYzM5NA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjA="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/domirando/Workspace/playground/flutterchi/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
