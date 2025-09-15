22f2c1d2c5fca53dc9541e00ee0509e7f8134997
Domirando <vohidjonovnamaftuna@gmail.com>
2025-09-15 12:43:55 +0500

    feat: basic ui

--- lib/main.dart
+++ lib/main.dart
@@ -8,57 +8,71 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    const String title = "Simple UI in Flutter";
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Simple Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(title)),
+        body: const Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              ImageNetwork(
+                alt: "nework owl image",
+                img:
+                    "https://media.istockphoto.com/id/1726264350/de/foto/vogelbeobachtung-von-waldohreulen-von-einem-kiefernzweig-in-einem-geheimnisvollen-wald-aus.webp?s=1024x1024&w=is&k=20&c=yCJn99tlasrzQ2HjP5HwQa7AR8kt7cx_jde16gs_eLo=",
+              ),
+              // ImageSection(image: 'images/lake.jpg'),
+              TitleSection(
+                topic: "Owl Photo",
+                description: "Taken in the US forest",
+              ),
+              TitleSection(topic: "Date", description: "2025-09-15"),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.topic,
+    required this.description,
+  });
+  final String topic;
+  final String description;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(topic, style: TextStyle(color: Colors.red[500])),
+        Padding(padding: EdgeInsets.all(16.0)),
+        Text(description, style: TextStyle(color: Colors.black)),
+      ],
+    );
+  }
+}
+
+class ImageNetwork extends StatelessWidget {
+  const ImageNetwork({super.key, required this.alt, required this.img});
+  final String alt;
+  final String img;
+  @override
+  Widget build(BuildContext context) {
+    return Image(image: NetworkImage(img));
+  }
+}
+
+// class ImageSection extends StatelessWidget {
+//   const ImageSection({super.key, required this.image});
+//
+//   final String image;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Image.asset(image, width: 600, height: 240, fit: BoxFit.cover);
+//   }
+// }
