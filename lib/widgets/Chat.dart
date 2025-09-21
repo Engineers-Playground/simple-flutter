9624c7c63bf2b6af89935e3f9bd029a2403cbc49
Domirando <vohidjonovnamaftuna@gmail.com>
2025-09-21 22:46:01 +0500

    feat: images added

--- lib/widgets/Chat.dart
+++ lib/widgets/Chat.dart
@@ -13,18 +13,28 @@ class Chat extends StatelessWidget {
           Expanded(
             flex: 1,
             child: Container(
-              padding: EdgeInsets.only(right: 8.0),
-              child: CircleAvatar(
-                // savol: shu image nega chiqmayapti?
-                backgroundImage: NetworkImage(
-                  scale: 1.0,
-                  "https://picsum.photos/250?image=9",
-                ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
               ),
+              padding: EdgeInsets.only(right: 8.0),
+              child: Image.network("https://picsum.photos/250?image=9"),
               // child: Image.network(
               //   "https://picsum.photos/seed/$chat_index/1200/700",
               // ),
             ),
+            // child: Container(
+            //   padding: EdgeInsets.only(right: 8.0),
+            //   child: CircleAvatar(
+            //     // savol: shu image nega chiqmayapti?
+            //     backgroundImage: NetworkImage(
+            //       scale: 1.0,
+            //       "https://picsum.photos/250?image=9",
+            //     ),
+            //   ),
+            //   // child: Image.network(
+            //   //   "https://picsum.photos/seed/$chat_index/1200/700",
+            //   // ),
+            // ),
           ),
           Expanded(
             flex: 7,
--- lib/widgets/PromoBanner.dart
+++ lib/widgets/PromoBanner.dart
@@ -6,47 +6,84 @@ class PromoBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      // decoration: BoxDecoration(image: Image(image: Image.asset("name"))),
-      padding: const EdgeInsets.all(10.0),
-      width: double.infinity,
-      child: Expanded(
-        child: Row(
-          children: [
-            Expanded(
-              flex: 4,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("YOUR GIFT IS RESERVED FOR:"),
-                  Text(
-                    "09:59",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Row(
-                      children: [Text("Get"), Icon(Icons.chevron_right)],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+    // return Container(
+    //   // decoration: BoxDecoration(image: Image(image: Image.asset("name"))),
+    //   padding: const EdgeInsets.all(10.0),
+    //   width: double.infinity,
+    //   child: Expanded(
+    //     child: Row(
+    //       children: [
+    //         Expanded(
+    //           flex: 4,
+    //           child: Column(
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: [
+    //               Text("YOUR GIFT IS RESERVED FOR:"),
+    //               Text(
+    //                 "09:59",
+    //                 style: TextStyle(
+    //                   fontSize: 20.0,
+    //                   color: Colors.black,
+    //                   fontWeight: FontWeight.bold,
+    //                 ),
+    //               ),
+    //             ],
+    //           ),
+    //         ),
+    //         Expanded(
+    //           flex: 1,
+    //           child: Column(
+    //             crossAxisAlignment: CrossAxisAlignment.end,
+    //             children: [
+    //               ElevatedButton(
+    //                 onPressed: () {},
+    //                 child: Row(
+    //                   children: [Text("Get"), Icon(Icons.chevron_right)],
+    //                 ),
+    //               ),
+    //             ],
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+    return Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          ),
+          width: double.infinity,
+          height: 150,
+          child: Image.network(
+            "https://picsum.photos/250?image=9",
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
+        Container(
+          padding: const EdgeInsets.all(5.0),
+          // alignment: Alignment.bottomCenter,
+          // decoration: BoxDecoration(
+          //   gradient: LinearGradient(
+          //     begin: Alignment.topCenter,
+          //     end: Alignment.bottomCenter,
+          //     colors: <Color>[
+          //       Colors.black.withAlpha(0),
+          //       Colors.black12,
+          //       Colors.black45,
+          //     ],
+          //   ),
+          // ),
+          child: const Text(
+            'Foreground Text',
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
+      ],
     );
   }
 }
